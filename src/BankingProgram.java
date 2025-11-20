438ad44cc3700472a13660b7fb1f92a459dfd17f
Mahammad <m.mammadov213@gmail.com>
2025-11-20 13:45:01 +0400

    changed the name of class from Main to BankingProgramm

--- src/BankingProgram.java
+++ src/BankingProgram.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 
-public class Main {
+public class BankingProgram {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
