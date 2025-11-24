edca1f32958fdcb3bae8959ba76dd2449af91d0e
Mahammad <m.mammadov213@gmail.com>
2025-11-24 15:30:57 +0400

    Rock Paper Scissors game added

---
+++ src/RockScissorsPaperGame.java
@@ -0,0 +1,48 @@
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockScissorsPaperGame {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        Random random = new Random();
+
+        String[] choices = {"rock", "paper", "scissors"};
+        String playerChoice;
+        String computerChoice;
+        String playAgain = "yes";
+
+        do {
+            System.out.print("Enter your move(rock , paper, scissors) : ");
+            playerChoice = scanner.nextLine().toLowerCase();
+
+            if (!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")){
+                System.out.println("invalid choice");
+                continue;
+            }
+
+            computerChoice = choices[random.nextInt(3)];
+            System.out.println("Computer choice: " + computerChoice);
+
+            if (playerChoice.equals(computerChoice)){
+                System.out.println("It's a tie");
+            } else if (playerChoice.equals("rock") && computerChoice.equals("scissors") ||
+                    (playerChoice.equals("paper") && computerChoice.equals("rock")) ||
+                    playerChoice.equals("scissors") && computerChoice.equals("paper")) {
+                System.out.println("You win");
+            }else {
+                System.out.println("You lose");
+            }
+
+            System.out.println("Play again (yen/no): ");
+            playAgain  =scanner.nextLine().toLowerCase();
+        }while (playAgain.equals("yes"));
+
+        System.out.println("Thanks for playing!");
+
+        scanner.close();
+    }
+}
